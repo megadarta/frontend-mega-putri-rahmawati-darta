7980b00d077f68d2ef47bcfc586d85822e3e45b6
megadarta <mega28102001@gmail.com>
2025-06-19 00:15:42 +0700

    darkmode

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react'
 import './App.css'
-import { Button, Col, Divider, Form, Input, InputNumber, Layout, message, Row, Select, Spin } from 'antd'
+import { Button, Col, ConfigProvider, Divider, Form, Input, InputNumber, Layout, message, Row, Select, Spin, theme } from 'antd'
 import axios from 'axios'
 import { dataValid, formatterDiskon, formatterRupiah, inputNumberProps, parserDiskon, parserRupiah, selectCommonProps } from './utils/formatUtils'
 const { TextArea } = Input;
@@ -86,7 +86,7 @@ function App() {
       .then((data) => {
         const options = data.data.map((item) => (
           {
-            label: item.id_barang + ' - ' +  item.nama_barang,
+            label: item.id_barang + ' - ' + item.nama_barang,
             value: item.id_barang,
             detail: item
           }
@@ -111,161 +111,185 @@ function App() {
     message.success('Order diproses')
   }
 
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
+    setIsDarkMode(mediaQuery.matches);
+
+    const handleChange = (e) => setIsDarkMode(e.matches);
+    mediaQuery.addEventListener('change', handleChange);
+
+    return () => {
+      mediaQuery.removeEventListener('change', handleChange);
+    };
+  }, []);
+
+
   return (
-    <div
-      style={{
-        minHeight: '100vh',
-        alignItems: 'center',
-        justifyContent: 'center',
-        padding: 24,
+    <ConfigProvider
+      theme={{
+        algorithm: isDarkMode ? theme.darkAlgorithm : theme.defaultAlgorithm,
       }}
     >
-      <Row gutter={[16, 16]} className='form-order'>
-        <Col xs={0} md={12} className='form-img'> </Col>
-        <Col xs={24} md={12} className='form-data'>
-          {loadingNegara ? <Divider orientation="center"><Spin tip="Loading..." /></Divider> :
-            <Form
-              form={form}
-              requiredMark
-              name="login"
-              initialValues={{
-                remember: true,
-              }}
-              size="medium"
-              layout="vertical"
-              onFinish={handleSubmit}
-            >
-              <Form.Item
-                name="negara"
-                label="Negara"
-                rules={[
-                  {
-                    required: true,
-                    message: "Tolong pilih negara!",
-                  },
-                ]}
-                style={{ marginBottom: "1rem" }}
+      <div
+        style={{
+          minHeight: '100vh',
+          alignItems: 'center',
+          justifyContent: 'center',
+          padding: 24,
+        }}
+      >
+        <Row gutter={[16, 16]} className='form-order'>
+          <Col xs={0} md={12} className='form-img'> </Col>
+          <Col xs={24} md={12} className='form-data'>
+            {loadingNegara ? <Divider orientation="center"><Spin tip="Loading..." /></Divider> :
+              <Form
+                form={form}
+                requiredMark
+                name="login"
+                initialValues={{
+                  remember: true,
+                }}
+                size="medium"
+                layout="vertical"
+                onFinish={handleSubmit}
               >
-                <Select
-                  {...selectCommonProps}
-                  loading={loadingNegara}
-                  placeholder="Pilih Negara"
-                  options={listNegara}
-                  onChange={(value) => updateListPelabuhan(value)}
-                />
-              </Form.Item>
-              <Form.Item
-                name="pelabuhan"
-                label="Pelabuhan"
-                rules={[
-                  {
-                    required: true,
-                    message: "Tolong pilih pelabuhan!",
-                  },
-                ]}
-                style={{ marginBottom: "1rem" }}
-              >
-                <Select
-                  {...selectCommonProps}
-                  disabled={!form.getFieldValue('negara')}
-                  placeholder="Pilih Pelabuhan"
-                  options={listPelabuhan}
-                  onChange={(value) => updateListBarang(value)}
-                />
-              </Form.Item>
-
-              <Form.Item
-                name="barang"
-                label="Barang"
-                rules={[
-                  {
-                    required: true,
-                    message: "Tolong pilih barang!",
-                  },
-                ]}
-                style={{ marginBottom: "1rem" }}
-              >
-                <Select
-                  {...selectCommonProps}
-                  disabled={!form.getFieldValue('pelabuhan')}
-                  placeholder="Pilih barang"
-                  options={listBarang}
-                  onChange={(_, data) => setSelectedBarang(data?.detail)}
-                />
-              </Form.Item>
-
-              <Form.Item
-                name="deskripsi"
-                label="Deskripsi"
-                style={{ marginBottom: "1rem" }}
-              >
-                <TextArea className="disable-text w-100'" disabled />
-              </Form.Item>
-
-              <Form.Item
-                name="diskon"
-                label="Diskon"
-                style={{ marginBottom: "1rem" }}
-              >
-                <InputNumber
-                  {...inputNumberProps}
-                  formatter={formatterDiskon}
-                  parser={parserDiskon}
-                  disabled={!selectedBarang}
-                  placeholder='Masukkan diskon'
-                  suffix="%"
-                  onChange={(value) => calculateTotal(value, 'diskon')}
-                />
-              </Form.Item>
-
-              <Form.Item
-                name="harga"
-                label="Harga"
-                rules={[
-                  {
-                    required: true,
-                    message: "Tolong masukkan harga!",
-                  },
-                ]}
-                style={{ marginBottom: "1rem" }}
-              >
-                <InputNumber
-                  disabled={!selectedBarang}
-                  placeholder='Masukkan harga'
-                  onChange={(value) => calculateTotal(value, 'harga')}
-                  {...inputNumberProps}
-                />
-
-              </Form.Item>
-
-              <Form.Item
-                name="total"
-                label="Total"
-                style={{ marginBottom: "1rem" }}
-              >
-                <InputNumber
-                  disabled
-                  {...inputNumberProps}
-                  className='disable-text w-100'
-                />
-
-              </Form.Item>
-
-              <Form.Item className="mt-4">
-                <Button
-                  block
-                  type="primary"
-                  htmlType="submit"
-                  loading={false}
+                <Form.Item
+                  name="negara"
+                  label="Negara"
+                  rules={[
+                    {
+                      required: true,
+                      message: "Tolong pilih negara!",
+                    },
+                  ]}
+                  style={{ marginBottom: "1rem" }}
                 >
-                  Order
-                </Button>
-              </Form.Item>
-            </Form>
-          }
-        </Col>
-      </Row>
-    </div>
+                  <Select
+                    {...selectCommonProps}
+                    loading={loadingNegara}
+                    placeholder="Pilih Negara"
+                    options={listNegara}
+                    onChange={(value) => updateListPelabuhan(value)}
+                  />
+                </Form.Item>
+                <Form.Item
+                  name="pelabuhan"
+                  label="Pelabuhan"
+                  rules={[
+                    {
+                      required: true,
+                      message: "Tolong pilih pelabuhan!",
+                    },
+                  ]}
+                  style={{ marginBottom: "1rem" }}
+                >
+                  <Select
+                    {...selectCommonProps}
+                    disabled={!form.getFieldValue('negara')}
+                    placeholder="Pilih Pelabuhan"
+                    options={listPelabuhan}
+                    onChange={(value) => updateListBarang(value)}
+                  />
+                </Form.Item>
+
+                <Form.Item
+                  name="barang"
+                  label="Barang"
+                  rules={[
+                    {
+                      required: true,
+                      message: "Tolong pilih barang!",
+                    },
+                  ]}
+                  style={{ marginBottom: "1rem" }}
+                >
+                  <Select
+                    {...selectCommonProps}
+                    disabled={!form.getFieldValue('pelabuhan')}
+                    placeholder="Pilih barang"
+                    options={listBarang}
+                    onChange={(_, data) => setSelectedBarang(data?.detail)}
+                  />
+                </Form.Item>
+
+                <Form.Item
+                  name="deskripsi"
+                  label="Deskripsi"
+                  style={{ marginBottom: "1rem" }}
+                >
+                  <TextArea
+                    className={(isDarkMode ? 'color-white' : 'disable-text') + ' w-100'}
+                    disabled
+                  />
+                </Form.Item>
+
+                <Form.Item
+                  name="diskon"
+                  label="Diskon"
+                  style={{ marginBottom: "1rem" }}
+                >
+                  <InputNumber
+                    {...inputNumberProps}
+                    formatter={formatterDiskon}
+                    parser={parserDiskon}
+                    disabled={!selectedBarang}
+                    placeholder='Masukkan diskon'
+                    suffix="%"
+                    onChange={(value) => calculateTotal(value, 'diskon')}
+                  />
+                </Form.Item>
+
+                <Form.Item
+                  name="harga"
+                  label="Harga"
+                  rules={[
+                    {
+                      required: true,
+                      message: "Tolong masukkan harga!",
+                    },
+                  ]}
+                  style={{ marginBottom: "1rem" }}
+                >
+                  <InputNumber
+                    disabled={!selectedBarang}
+                    placeholder='Masukkan harga'
+                    onChange={(value) => calculateTotal(value, 'harga')}
+                    {...inputNumberProps}
+                  />
+
+                </Form.Item>
+
+                <Form.Item
+                  name="total"
+                  label="Total"
+                  style={{ marginBottom: "1rem" }}
+                >
+                  <InputNumber
+                    disabled
+                    {...inputNumberProps}
+                    className={(isDarkMode ? 'color-white' : 'disable-text') + ' w-100'}
+                  />
+
+                </Form.Item>
+
+                <Form.Item className="mt-4">
+                  <Button
+                    block
+                    type="primary"
+                    htmlType="submit"
+                    loading={false}
+                  >
+                    Order
+                  </Button>
+                </Form.Item>
+              </Form>
+            }
+          </Col>
+        </Row>
+      </div>
+    </ConfigProvider>
   )
 }
 
